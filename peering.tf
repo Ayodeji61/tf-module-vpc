0294819a7385ac363e5e64405f1537d77d7553c1
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-14 02:52:00 -0700

    Updating

--- peering.tf
+++ peering.tf
@@ -3,4 +3,5 @@ resource "aws_vpc_peering_connection" "management-vpc-to-env-vpc" {
   peer_vpc_id   = lookup(var.management_vpc, "vpc_id", null)
   vpc_id        = element([for k, v in aws_vpc.main : v.id], 0)
   auto_accept   = true
+  tags          = local.vpc_peering_tags
 }
