cd135616cb422b2a81ba9bacc7ccd803115c996a
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-14 02:57:21 -0700

    Updating

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@ locals {
     ENV = var.env
     PROJECT = "roboshop"
   }
-  vpc_peering_tags {
+  vpc_peering_tags = {
     Name = "${var.env}-vpc-to-management-vpc"
     ENV = var.env
     PROJECT = "roboshop"
