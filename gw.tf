2557cbf81204554b0fcd9e6b3d33662339d10cd6
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-15 14:03:59 -0700

    Updating

--- gw.tf
+++ gw.tf
@@ -24,7 +24,7 @@ resource "aws_nat_gateway" "ngw" {
 
 locals {
   private_route_tables = flatten([for i, j in module.private_subnets : j.rt])
-  private_subnets_list = flatten([for i, j in module.private_subnets : j.subnets_list])
+
   public_subnets_list = flatten([for i, j in module.public_subnets : j.subnets_list])
   public_route_tables = flatten([for i, j in module.public_subnets : j.rt])
 }
