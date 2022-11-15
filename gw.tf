8bd5745a93307e6704f1172a0e4713426ec196fb
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-15 11:33:33 -0700

    Updating

--- gw.tf
+++ gw.tf
@@ -11,3 +11,14 @@ resource "aws_eip" "ngw" {
   vpc = true
 }
 
+locals {
+  private_route_tables = [for i,j in module.private_subnets : j.rt]
+  public_route_tables = [for i,j in module.public_subnets : j.rt]
+}
+
+resource "aws_route" "internet_gateway_route_to_public_subnets" {
+  count = length(local.public_route_tables)
+  route_table_id            = element(local.public_route_tables, count.index)
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.gw.id
+}
