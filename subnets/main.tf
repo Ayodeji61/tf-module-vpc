f2eed5e8a46fccc0254e34cbac685e9a1ab480e1
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-15 13:47:49 -0700

    Updating

--- subnets/main.tf
+++ subnets/main.tf
@@ -19,20 +19,20 @@ resource "aws_route_table" "aws_route_table" {
   }
 }
 
-resource "aws_route" "peering_connection_route" {
-  for_each = var.subnets
-  route_table_id = lookup(lookup(aws_route_table.aws_route_table, each.value.name, null), "id", null)
-  destination_cidr_block = lookup(var.management_vpc, "vpc_cidr", null)
-  vpc_peering_connection_id = var.peering_connection_id
-}
+//resource "aws_route" "peering_connection_route" {
+//  for_each = var.subnets
+//  route_table_id = lookup(lookup(aws_route_table.aws_route_table, each.value.name, null), "id", null)
+//  destination_cidr_block = lookup(var.management_vpc, "vpc_cidr", null)
+//  vpc_peering_connection_id = var.peering_connection_id
+//}
 
 locals {
   subnets_list = flatten([for i, j in module.lm-subnets : j.subnets])
 }
 
-//output "subnets_list" {
- // value = local.subnets_list[0].id
-//}
+output "subnets_list" {
+  value = local.subnets_list[*].id
+}
 
 output "rt" {
   value = [for i, j in aws_route_table.aws_route_table : j.id]
