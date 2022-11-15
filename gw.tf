06901a01d32241c4b696523c2673fa323a0f544e
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-15 14:06:58 -0700

    Updating

--- gw.tf
+++ gw.tf
@@ -24,7 +24,7 @@ resource "aws_nat_gateway" "ngw" {
 
 locals {
   private_route_tables = flatten([for i, j in module.private_subnets : j.rt])
-
+  private_subnets_list = flatten([for i, j in module.private_subnets : j.subnets_list])
   public_subnets_list = flatten([for i, j in module.public_subnets : j.subnets_list])
   public_route_tables = flatten([for i, j in module.public_subnets : j.rt])
 }
@@ -36,10 +36,10 @@ resource "aws_route" "internet_gateway_route_to_public_subnets" {
   gateway_id = aws_internet_gateway.gw[0].id
 }
 
-//resource "aws_route" "internet_gateway_route_to_private_subnets" {
-//  count = length(local.private_route_tables)
- // route_table_id            = element(local.private_route_tables, count.index)
-//  destination_cidr_block    = "0.0.0.0/0"
-//  gateway_id = aws_nat_gateway.nw
-//}
+resource "aws_route" "internet_gateway_route_to_private_subnets" {
+ count = length(local.private_route_tables)
+  route_table_id            = element(local.private_route_tables, count.index)
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_nat_gateway.ngw[0].id
+}
 
