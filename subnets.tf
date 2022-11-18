4d8cbfa706fdabb00829710bc606539c50252cfe
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-18 01:06:54 -0700

    Updating

--- subnets.tf
+++ subnets.tf
@@ -19,7 +19,7 @@ module "public_subnets" {
   cidr_block = each.value.cidr_block
   vpc_id = aws_vpc.main.id
   env    = var.env
-  name    = var.name
+  name    = each.value.name
   subnet_availability_zones = var.subnet_availability_zones
   //peering_connection_id = aws_vpc_peering_connection.management-vpc-to-env-vpc.*.id[0]
 // management_vpc = var.management_vpc
