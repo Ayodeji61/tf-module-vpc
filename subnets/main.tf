317f403a3f0a01165ab9b25c819ab0aef55d4180
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-18 01:13:51 -0700

    Updating

--- subnets/main.tf
+++ subnets/main.tf
@@ -40,10 +40,9 @@ resource "aws_route_table" "aws_route_table" {
   }
 }
 
-/*
+
 resource "aws_route_table_association" "route-table-association" {
   count = length(aws_subnet.main)
   subnet_id = element(aws_subnet.main.*.id, count.index)
-  route_table_id = var.route_table_id
+  route_table_id = aws_route_table.aws_route_table.id
 }
-*/
